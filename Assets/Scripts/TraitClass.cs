00760d465ef1c616bde7482bccbb608849441292
alecphobia95 <46802114+alecphobia95@users.noreply.github.com>
2021-01-07 03:10:57 -0500

    Added trait skeleton

---
+++ Assets/Scripts/TraitClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraitClass
+{
+    public string traitName;
+    public float traitStrength;
+
+    public TraitClass(string name)
+    {
+        traitName = name;
+    }
+
+    public TraitClass(string name, float strength)
+    {
+        traitName = name;
+        traitStrength = strength;
+    }
+}
+
+public class TraitList: MonoBehaviour
+{
+    TraitClass sociable = new TraitClass("sociable");
+    
+}
